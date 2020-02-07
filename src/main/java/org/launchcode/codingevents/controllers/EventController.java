0229aeda5374a62b0021f625e7311b00110371e2
cjconnor18 <cjallen1988@gmail.com>
2020-02-06 18:45:32 -0600

    starting exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -15,14 +17,13 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    Map<String, String> events = new HashMap<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+            events.put("Coding and Cocktails", "Drink and learn to code.");
+            events.put("LaunchCode", "Learn coding for free, two nights a week!");
+            events.put("Kansas City Women in Technology", "Meet other women in technology careers.")
         return "events/index";
     }
 
